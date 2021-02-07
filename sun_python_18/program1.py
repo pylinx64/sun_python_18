38b16311e6acfafd848c7598cc76cd3da08ac6e2
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-07 20:02:53 +0200

    Add files via upload

---
+++ sun_python_18/program1.py
@@ -0,0 +1,7 @@
+print('Доллар по 8 !!')
+
+import time
+while True:
+	print('Wzlom')
+	print('...')
+	time.sleep(0.5)
---
+++ sun_python_18/program2.py
@@ -0,0 +1,40 @@
+#name = input('Введите пароль ')
+#print('Привет '+name+'! ')
+
+print(10)
+print(10+10)
+print('10+10')
+print(10-100)
+print(10/25)
+print(10*10)
+
+print((2+2)*2)
+
+print('a'+'b')
+#print('a ' * 9)
+
+x = 11
+print(x)
+x = 15
+print(x)
+
+x = 'Текст123'
+print(x)
+
+z = 10
+x = 11
+y = 20
+print(x+y+z)
+
+text1 = 'Галичина '
+moloko_1 = "Яготинское"
+print(text1+moloko_1)
+
+number1 = input('Введите число 1: ')
+number2 = input('Введите число 2: ')
+number3 = input('Введите число 3: ')
+print(int(number1) + int(number2) - int(number3))
+
+#i = 1
+
+
