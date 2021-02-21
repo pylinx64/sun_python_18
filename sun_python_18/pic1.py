3c1ce523702d1f322224103bad24a128a6106972
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-21 20:03:54 +0200

    Add files via upload

---
+++ sun_python_18/pic1.py
@@ -0,0 +1,13 @@
+import turtle
+import time
+
+t = turtle.Pen()
+#---------------
+turtle.bgcolor('black')
+t.pencolor('lime')
+t.forward(100) 
+t.left(45)
+t.forward(100)
+
+#---------------
+time.sleep(100)
---
+++ sun_python_18/program4.py
@@ -0,0 +1,5 @@
+import colorama
+from colorama import Fore
+
+colorama.init()
+print(Fore.CYAN+'Hello wolrd')
