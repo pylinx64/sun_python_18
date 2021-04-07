4e5531aca640dc6caae7894396262c407666e33f
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-07 11:35:24 +0300

    Add files via upload

--- sun_python_18/bot.py
+++ sun_python_18/bot.py
@@ -1,2 +1,13 @@
-#1710188888:AAG_fM6e1hF7pW6IAVhLCAYVjOhScuMpIhs
+import telebot
 
+bot = telebot.TeleBot('1710188888:AAG_fM6e1hF7pW6IAVhLCAYVjOhScuMpIhs')
+
+@bot.message_handler(commands=['start'])
+def start_text(message):
+    print(message.chat.first_name, message.text)
+    bot.send_message(message.chat.id, '🥡Привет, я чат-бот с колбасным тортом!')
+    
+
+    
+print('#bot run...')
+bot.polling()
