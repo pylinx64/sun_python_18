b73b3f55f12d82c10983be2310ad7844dd98335e
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-11 20:31:57 +0300

    Add files via upload

--- sun_python_18/bot.py
+++ sun_python_18/bot.py
@@ -1,5 +1,7 @@
+
 import telebot
 
+# токен
 bot = telebot.TeleBot('1710188888:AAG_fM6e1hF7pW6IAVhLCAYVjOhScuMpIhs')
 
 @bot.message_handler(commands=['start'])
@@ -7,7 +9,13 @@ def start_text(message):
     print(message.chat.first_name, message.text)
     bot.send_message(message.chat.id, '🥡Привет, я чат-бот с колбасным тортом!')
     
-
+@bot.message_handler(content_types=['text'])
+def message_text(message):
+    # отправляет сообщение
+    if message.text == 'привет':
+        bot.send_message(message.chat.id, 'Ну хай')
+    else:
+        bot.send_message(message.chat.id, 'Я тебя не понял')
     
 print('#bot run...')
 bot.polling()
